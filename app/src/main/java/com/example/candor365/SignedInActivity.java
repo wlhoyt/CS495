9082d163a95ae04301561136774609d01a1211c1
jstringf <josh.stringfellow@adtran.com>
2020-02-11 22:15:31 -0600

    seems stable

--- app/src/main/java/com/example/candor365/SignedInActivity.java
+++ app/src/main/java/com/example/candor365/SignedInActivity.java
@@ -24,6 +24,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
@@ -52,6 +53,22 @@ import butterknife.OnClick;
 import static com.firebase.ui.auth.AuthUI.EMAIL_LINK_PROVIDER;
 
 public class SignedInActivity extends AppCompatActivity {
+    //final Button btnLogOut = (Button) findViewById(R.id.sign_out);
+
+//    public void onClick(View v) {
+//        if (v.getId() == R.id.sign_out) {
+//            AuthUI.getInstance()
+//                    .signOut(this)
+//                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                        public void onComplete(@NonNull Task<Void> task) {
+//                            // user is now signed out
+//                            //startActivity(new Intent(SignedInActivity.this, SignedInActivity.class));
+//                            finish();
+//                        }
+//                    });
+//        }
+//    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
