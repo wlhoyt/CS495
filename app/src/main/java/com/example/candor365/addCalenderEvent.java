d2418840a90eac783ce322885b7d6a6b610ce367
dcnix <dcnix@crimson.ua.edu>
2020-02-23 22:05:23 -0600

    add event progress

--- app/src/main/java/com/example/candor365/addCalenderEvent.java
+++ app/src/main/java/com/example/candor365/addCalenderEvent.java
@@ -3,12 +3,76 @@ package com.example.candor365;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Map;
 
 public class addCalenderEvent extends AppCompatActivity {
+
+    private EditText eventName;
+    private EditText eventDate;
+    private EditText eventDescr;
+    String event_title = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_calender_event);
+        eventName  = (EditText) findViewById(R.id.event_name);
+        eventDate  = (EditText) findViewById(R.id.event_date);
+        eventDescr = (EditText) findViewById(R.id.event_description);
+        eventName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //do nothing
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //do nothing
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //save the text to write to the db on save event button click
+                
+            }
+        });
+//        eventDate.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                //do nothing
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                //do nothing
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                //save text to write to the db on save event button click
+//            }
+//        });
+//        eventDescr.addTextChangedListener(new TextWatcher() {
+//            @Override
+//            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                //do nothing
+//            }
+//
+//            @Override
+//            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+//                //do nothing
+//            }
+//
+//            @Override
+//            public void afterTextChanged(Editable editable) {
+//                //save text to write to db on save event click
+//            }
+//        });
+
     }
 }
