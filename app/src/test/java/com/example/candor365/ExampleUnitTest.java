07bd849ae18ffe0a1530659aca66348ca819862c
Jason Graham <jagraham2@crimson.ua.edu>
2020-03-27 21:33:24 -0500

    Starting to unit testing, not working right now

--- app/src/test/java/com/example/candor365/ExampleUnitTest.java
+++ app/src/test/java/com/example/candor365/ExampleUnitTest.java
@@ -1,6 +1,11 @@
 package com.example.candor365;
 
+import junit.extensions.ActiveTestSuite;
+
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.Assert.*;
 
@@ -13,5 +18,8 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+    public void something(){
+
     }
 }
