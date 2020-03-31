7f3f7e966f936007fb4c4d7a0adad9802ce76809
Jason Graham <jagraham2@crimson.ua.edu>
2020-03-31 17:26:54 -0500

    added unit test file

---
+++ app/src/test/java/com/example/candor365/unitTests.java
@@ -0,0 +1,14 @@
+package com.example.candor365;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class unitTests {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+    public void something(){}
+
+}
