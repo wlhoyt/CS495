c446c90130b0b133613a29091c525dffbe06c454
dnix4797 <dnix4797@gmail.com>
2020-04-01 15:46:56 -0500

    firebase testing files

--- app/src/main/java/com/example/candor365/FirebaseMocker.java
+++ app/src/main/java/com/example/candor365/FirebaseMocker.java
@@ -1,5 +1,21 @@
 package com.example.candor365;
 
-public class FirebaseMocker {
+import android.content.Context;
+import com.firebase.client.Firebase;
+import java.util.concurrent.atomic.AtomicBoolean;
 
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.when;
+
+public class FirebaseMocker {
+    @Mock
+    Context context;
+    @Mock
+    Context appContext;
+    @Mock
+    private android.content.SharedPreferences sharedPreferences;
+
+    private static AtomicBoolean initialized = new AtomicBoolean(false);
+    private Firebase firebase;
 }
